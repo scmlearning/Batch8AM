b6c46f0b1a3ad25ff4ac9d3b5f7c7bd8839eb666
naren <naren.test@gmail.com>
2019-04-19 08:51:05 +0530

    Created this for ini project

---
+++ Logout.java
@@ -0,0 +1,8 @@
+class Logout
+{
+        public static void main (String args[])
+        {
+                System.out.println("Hello rhs is my logout java progream");
+        }
+}
+
---
+++ Payment.java
@@ -0,0 +1,8 @@
+class Payment
+{
+        public static void main (String args[])
+        {
+                System.out.println("Hello rhs is my Payment java progream");
+        }
+}
+
